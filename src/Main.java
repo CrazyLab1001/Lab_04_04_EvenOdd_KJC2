e4dac2b54a46b8cf2e8e39168b4bca8ac947c388
CrazyLab1001 <cramerkj@mail.uc.edu>
2025-02-02 10:58:11 -0500

    Task 4 completed (awesomely)!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int numberToExamine = 32;
+        if (numberToExamine % 2 == 0)
+            System.out.println("Your number is even!");
+        else
+            System.out.println("Your number is odd!");
+
     }
 }
